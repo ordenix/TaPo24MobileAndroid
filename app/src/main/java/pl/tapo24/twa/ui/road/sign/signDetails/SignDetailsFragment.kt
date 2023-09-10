2b262e21330dfd1a52425a4170b4f2762b2a38ba
Jakub Drzymała <ordenix@otavi.pl>
2023-09-10 21:21:58 +0200

    fix layout sign details

--- app/src/main/java/pl/tapo24/twa/ui/road/sign/signDetails/SignDetailsFragment.kt
+++ app/src/main/java/pl/tapo24/twa/ui/road/sign/signDetails/SignDetailsFragment.kt
@@ -11,6 +11,7 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import pl.tapo24.twa.databinding.FragmentSignDetailsBinding
+import pl.tapo24.twa.dbData.entity.Sign
 import java.io.File
 
 
@@ -20,6 +21,15 @@ class SignDetailsFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var viewModel: SignDetailsViewModel
 
+    lateinit var signDetails: Sign
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val args = arguments?.let { SignDetailsFragmentArgs.fromBundle(it) }
+        signDetails = args!!.signDetails
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,13 +37,16 @@ class SignDetailsFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(SignDetailsViewModel::class.java)
         _binding = FragmentSignDetailsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val file: File = File(context?.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "test/a1.png")
+        val file: File = File(context?.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), signDetails.path!!)
         if (file.exists()) {
             val myBitmap = BitmapFactory.decodeFile(file.absolutePath)
 
             val myImage = binding.imageView16
 
             myImage.setImageBitmap(myBitmap)
+
+            binding.signDescr.text = signDetails.description
+            binding.signTitle.text = "Szczegóły znaku ${signDetails.name}"
         }
 
 
