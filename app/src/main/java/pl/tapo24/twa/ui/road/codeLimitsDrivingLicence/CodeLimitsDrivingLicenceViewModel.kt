09124b7a942fa83fae9b7128fee1dafbbf5a4af6
Jakub Drzymała <ordenix@otavi.pl>
2024-02-11 17:24:23 +0100

    implement colorSpan

--- app/src/main/java/pl/tapo24/twa/ui/road/codeLimitsDrivingLicence/CodeLimitsDrivingLicenceViewModel.kt
+++ app/src/main/java/pl/tapo24/twa/ui/road/codeLimitsDrivingLicence/CodeLimitsDrivingLicenceViewModel.kt
@@ -12,14 +12,15 @@ import kotlinx.coroutines.withContext
 import pl.tapo24.twa.adapter.CodeLimitsDrivingLicenceAdapter
 import pl.tapo24.twa.dbData.DataTapoDb
 import pl.tapo24.twa.dbData.entity.CodeLimitsDrivingLicence
+import pl.tapo24.twa.utils.ColorSpan
 import pl.tapo24.twa.utils.SpanFilter
 import javax.inject.Inject
 
 @HiltViewModel
 class CodeLimitsDrivingLicenceViewModel @Inject constructor(
     private val dataTapoDb: DataTapoDb
-): ViewModel() {
-    var searchText: String = ""
+): ViewModel(), ColorSpan {
+    override var searchText: String = ""
     val data = MutableLiveData<List<CodeLimitsDrivingLicence>>()
     lateinit var adapter: CodeLimitsDrivingLicenceAdapter
 
@@ -36,7 +37,4 @@ class CodeLimitsDrivingLicenceViewModel @Inject constructor(
 
     }
 
-    fun getSpanText(text: String?): SpannableString? {
-        return SpanFilter().getSimpleSpanText(text, searchText)
-    }
 }
