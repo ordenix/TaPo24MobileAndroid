e4ff194b28911d2b120c8adf8c172ef5200433e6
Jakub Drzymala <MY_NAME@example.com>
2024-05-16 16:18:01 +0200

    Add documentation methods in DataFormatterConverter

--- app/src/main/java/pl/tapo24/twa/utils/DataFormatterConverter.kt
+++ app/src/main/java/pl/tapo24/twa/utils/DataFormatterConverter.kt
@@ -6,6 +6,14 @@ interface DataFormatterConverter {
 
 
 
+    /**
+     * Formats the given year, month, and dayOfMonth into a human-readable calendar date.
+     *
+     * @param year The year component of the date.
+     * @param _month The month component of the date. (0 - 11)
+     * @param dayOfMonth The day of the month component of the date. (1 - 31)
+     * @return A string representation of the calendar date in the format "yyyy-MM-dd".
+     */
     fun formatToHumanCalendarDate(year: Int, _month: Int, dayOfMonth: Int): String {
         val month = _month + 1
         var monthString = ""
@@ -26,6 +34,12 @@ interface DataFormatterConverter {
     }
 
 
+    /**
+     * Converts a human-readable calendar date string in the format "yyyy-MM-dd" to a timestamp.
+     *
+     * @param humanDate The human-readable calendar date string to convert.
+     * @return The timestamp corresponding to the provided humanDate.
+     */
     fun convertHumanCalendarDateToTimeStamp(humanDate: String) : Int {
         val date = humanDate.split("-")
         val year = date[0].toInt()
@@ -37,6 +51,12 @@ interface DataFormatterConverter {
 
     }
 
+    /**
+     * Converts a timestamp to a human-readable calendar date string.
+     *
+     * @param timeStamp The timestamp to convert.
+     * @return The human-readable calendar date string in the format "yyyy-MM-dd".
+     */
     fun convertTimeStampToHumanCalendarDate(timeStamp: Int): String {
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = timeStamp.toLong()*1000
